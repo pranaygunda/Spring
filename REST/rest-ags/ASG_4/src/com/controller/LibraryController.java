73651ac99aafd8ec320f8bb78f90d37c8b63a9ec
pranaygunda <pranaygunda1998@gmail.com>
2020-11-17 10:18:02 +0530

    commited

--- REST/rest-ags/ASG_4/src/com/controller/LibraryController.java
+++ REST/rest-ags/ASG_4/src/com/controller/LibraryController.java
@@ -1,31 +1,37 @@
 package com.controller;
 
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.model.BookData;
 
 @RestController
 @RequestMapping("/book")
 public class LibraryController {
+	static ArrayList<BookData> book= new ArrayList<BookData>();
 	
-		BookData b1 = new BookData(1,"Python",2010);
-		BookData b2 = new BookData(2,"DevOps",2015);
-		BookData b3 = new BookData(3,"C++",2000);
-		BookData b4 = new BookData(4,"BlockChain",1999);
-		BookData b5 = new BookData(5,"Angular",2012);
-		
-		BookData b[] = {b1,b2,b3,b4,b5};
+	public static void createBook() {
+			 BookData b1 = new BookData(1,"Python",2010);
+			 BookData b2 = new BookData(2,"DevOps",2015);
+			 BookData b3 = new BookData(3,"C++",2000);
+			 BookData b4 = new BookData(4,"BlockChain",1999);
+			 BookData b5 = new BookData(5,"Angular",2012);
+			Collections.addAll(book, b1,b2,b3,b4,b5);
+		}
+
 	
 	
 	@RequestMapping("/displayAll")
 	public String getBookDetails() {
+		createBook();
+		System.out.println("Created");
 		String o="";
-		 for(BookData b1 : b)
+		 for(BookData b1 : book)
 		 {
-            o+="id:"+b1.getId()+"\tname"+b1.getName()+"\tyear"+b1.getYear()+"\n";
+            o+= "id: "+b1.getId()+"\tname"+b1.getName()+"\tyear"+b1.getYear()+"\n";
 		 }
 		return o;
 	}
@@ -33,7 +39,7 @@ public class LibraryController {
 	@RequestMapping("/{id}")
 	public String getBookDetails(@PathVariable int id) {
 		String o="";
-		for(BookData b1: b)
+		for(BookData b1: book)
 		{
 			if(b1.getId()==id)
 			{
